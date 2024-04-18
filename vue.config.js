46b6e42d96213f5dfb7bf7f999ddeb26aa1b5335
emineyalman <emine.yalman.357@gmail.com>
2024-04-17 22:14:12 -0500

    portpolio added

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("define").tap((definitions) => {
+      definitions[0].__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
+      return definitions;
+    });
+  },
   pluginOptions: {
-    configureWebpack: {
-      plugins: [],
-    },
-    chainWebpack: (config) => {
-      config.plugin("define").tap((definitions) => {
-        definitions[0].__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = false;
-        return definitions;
-      });
-    },
-    },
     vuetify: {
       theme: {
         dark: true,
